2a69cb2c85f62ac0bdc2b3a8ce30d1487f5f132d
Gassama <gassma803@gmail.com>
2024-01-12 00:49:24 +0100

    debut du javaScript, après avoir revu le html et Sass

---
+++ main.js
@@ -0,0 +1,7 @@
+//quand un utilisateur viens sur notre page il a le bouton jouer sur le quel il peut cliquer pour commencer le jeu
+//si il clique:
+     //et qu'il ne jouait pas:
+         // le compteur caché avec display none doit s'afficher et le compteur decremente de 60 à 0 seconde après seconde
+            //si on arrive à 0 sec, Game over s'affiche qui est initi
+         //le nom de notre bouton jouer change pour 'Relancer' s'il veut reprendre la partie
+         //
